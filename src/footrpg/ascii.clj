2f91c57a7107630be5e12b644c5ddf2cc3793aa4
Scott Merritt <srmerritt88@gmail.com>
2018-10-28 23:55:56 -0700

    can move the cursor around the pitch now

--- src/footrpg/ascii.clj
+++ src/footrpg/ascii.clj
@@ -4,21 +4,33 @@
 
 (def ascii-pitch (string/split-lines (slurp "resources/ascii-pitch.txt")))
 
-(defn make-ui []
-  {})
-
-(defn ui-loop []
-  )
-
-(defn redraw [ui]
-  )
+(def screen)
 
 ;; Probably should bring in stuart sierra component shortly
-(defn main []
-  (let [screen (s/get-screen :text)]
-    (s/in-screen screen
-                 (doseq [[i line] (map-indexed vector ascii-pitch)]
-                   (s/put-string screen 0 i line))
-                 (s/redraw screen)
-                 (s/get-key-blocking screen)
-                 (s/stop screen))))
+(defn init []
+  (def screen (s/get-screen :text))
+  (s/start screen)
+  ;; Hack recommended in a clojure-lanterna issue https://github.com/sjl/clojure-lanterna/issues/7
+  (s/get-key screen)
+  (s/redraw screen))
+
+(defn transpose-cursor [[x y] pitch]
+  [(-> x (* 2) inc)
+   (inc y)])
+
+(defn redraw [state]
+  (doseq [[i line] (map-indexed vector ascii-pitch)]
+    (s/put-string screen 0 i line))
+  (let [[curs-x curs-y] (transpose-cursor (:cursor state) (:pitch state))]
+    (s/move-cursor screen curs-x curs-y))
+  (s/redraw screen))
+
+(defn input []
+  (s/get-key-blocking screen))
+
+(defn stop []
+  (s/stop screen))
+
+;; API scratch
+;; move cursor
+;; select tile
--- src/footrpg/core.clj
+++ src/footrpg/core.clj
@@ -2,7 +2,7 @@
   (:require [footrpg.ascii :as ascii])
   (:gen-class))
 
-(defn make-state []
+(defn make-game-state []
   {:teams {:home nil
            :away nil}
    :ball nil
@@ -10,13 +10,29 @@
            :away 0}})
 
 (defn make-pitch []
-  {:width 56
+  {:width 58
    :height 37})
+
+(defn pitch-center [p]
+  [(-> (:width p) (/ 2))
+   (-> (:height p) (/ 2))])
+
+(defn pitch-top-right [p]
+  [(dec (:width p)) 0])
+
+(defn pitch-bottom-right [p]
+  [(dec (:width p))
+   (dec (:height p))])
+
+(defn pitch-top-left [p]
+  [0 0])
+
+(defn pitch-bottom-left [p]
+  [0 (dec (:height p))])
 
 (defn make-location []
   {:vect nil
-   :x nil
-   :y nil})
+   :loc []})
 
 (defn make-ball []
   {:kind :ball
@@ -30,17 +46,76 @@
    :location (make-location)}
   )
 
-(defn add-vect [location vect]
+(defn add-vect [location [dx dy]]
   (-> location
-      (assoc :vect vect)
-      (update :x + (:x vect))
-      (update :y + (:y vect))))
+      (assoc :vect [dx dy])
+      (update-in [:loc 0] + dx)
+      (update-in [:loc 1] + dy)))
 
 (defn move [thing vect]
   (update thing :location add-vect vect))
 
+;; This is now meta-game
+
+(defn bounded [[x y] pitch]
+  [(-> x (max 0) (min (dec (:width pitch))))
+   (-> y (max 0) (min (dec (:height pitch))))])
+
+(defn cursor-left [s]
+  (-> s
+    (update-in [:cursor 0] dec)
+    (update :cursor bounded (:pitch s))))
+
+(defn cursor-right [s]
+  (-> s
+    (update-in [:cursor 0] inc)
+    (update :cursor bounded (:pitch s))))
+
+(defn cursor-up [s]
+  (-> s
+    (update-in [:cursor 1] dec)
+    (update :cursor bounded (:pitch s))))
+
+(defn cursor-down [s]
+  (-> s
+    (update-in [:cursor 1] inc)
+    (update :cursor bounded (:pitch s))))
+
+(def main-mode {:left cursor-left
+                :right cursor-right
+                :up cursor-up
+                :down cursor-down
+                :escape (constantly :exit)
+                \q (constantly :exit)})
+
+(defn make-state []
+  {:cursor [0 0]})
+
+(def state (make-state))
+
+(defn init []
+  (let [pitch (make-pitch)]
+    (def state (-> state
+                   (assoc :pitch pitch)
+                   (assoc :mode main-mode)
+                   (assoc :cursor (pitch-center pitch))))))
+
+(defn main-loop []
+  (loop [s state]
+    (ascii/redraw s)
+    (let [c (ascii/input)
+          f ((:mode s) c)
+          s2 (if (nil? f) s (apply f [s]))]
+      (if (= :exit s2)
+        nil
+        (recur s2)))))
+
 (defn main [ui-type]
-  (ascii/main))
+  (ascii/init)
+  (try
+    (init)
+    (main-loop)
+    (finally (ascii/stop))))
 
 (defn -main [& args]
   (main :ascii))
